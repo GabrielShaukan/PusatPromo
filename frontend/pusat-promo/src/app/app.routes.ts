09ee46dc1b207b2f9b3b29d4233105733cb0a8e4
DESKTOP-VROCIO1\gabri <gabrielshaukan@yahoo.com>
2026-03-08 20:18:43 +0100

    PP-4:Connect bakend health endpoint with frontend

--- frontend/pusat-promo/src/app/app.routes.ts
+++ frontend/pusat-promo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'health', pathMatch: 'full' },
+	{ path: 'health', loadComponent: () => import('./pages/health/health.component').then(m => m.HealthComponent) }
+];
